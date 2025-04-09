888fab4ba6d155a7a90b9ec8b30f0b84951f6428
Amanda Montarroios <amontarroiosoliveira@gmail.com>
2025-04-09 08:48:00 -0300

    nova base

---
+++ src/br/edu/cs/poo/ac/seguro/entidades/SeguradoEmpresa.java
@@ -0,0 +1,49 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SeguradoEmpresa extends Segurado {
+    private String cnpj;
+    private double faturamento;
+    private boolean ehLocadoraDeVeiculos;
+
+    public SeguradoEmpresa(String nome, Endereco endereco, LocalDate dataAbertura, BigDecimal bonus, String cnpj, double faturamento, boolean ehLocadoraDeVeiculos){
+        super(nome, endereco, dataAbertura, bonus);
+        this.cnpj = cnpj;
+        this.faturamento = faturamento;
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
+
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj(){
+        return cnpj;
+    }
+
+    public void setFaturamento(double faturamento){
+        this.faturamento = faturamento;
+    }
+
+    public double getFaturamento(){
+        return faturamento;
+    }
+
+    public void setEhLocadoreDeVeiculos(boolean ehLocadoraDeVeiculos){
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
+
+    public boolean getEhLocadoraDeVeiculos(){
+        return ehLocadoraDeVeiculos;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura){
+        setDataCriacao(dataAbertura);
+    }
+
+    public LocalDate getDataAbertura(){
+        return getDataCriacao();
+    }
+}
---
+++ src/br/edu/cs/poo/ac/seguro/entidades/SeguradoPessoa.java
@@ -0,0 +1,39 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SeguradoPessoa extends Segurado {
+    private String cpf;
+    private double renda;
+
+    public SeguradoPessoa(String nome, Endereco endereco, LocalDate dataNascimento, BigDecimal bonus, String cpf, double renda){
+        super(nome, endereco, dataNascimento, bonus);
+        this.cpf = cpf;
+        this.renda = renda;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setRenda(double renda){
+        this.renda = renda;
+    }
+
+    public double getRenda(){
+        return renda;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento){
+        setDataCriacao(dataNascimento);
+    }
+
+    public LocalDate getDataNascimento(){
+        return getDataCriacao();
+    }
+}
